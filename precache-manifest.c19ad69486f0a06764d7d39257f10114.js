9201ccf86b37b9fe3afb123c2cf839b7d81af480
Can Ural <can.ural@outlook.com>
2019-11-17 13:57:28 +0300

    Updates

---
+++ precache-manifest.c19ad69486f0a06764d7d39257f10114.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d65aba6d6413a9389c04ea2cd5ae4c37",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "eb2e83d34c498388c016",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "98fdac62b2de2f9943ba",
+    "url": "/robofriends/static/css/main.cf346a26.chunk.css"
+  },
+  {
+    "revision": "eb2e83d34c498388c016",
+    "url": "/robofriends/static/js/2.591f2f3b.chunk.js"
+  },
+  {
+    "revision": "98fdac62b2de2f9943ba",
+    "url": "/robofriends/static/js/main.af3262f3.chunk.js"
+  },
+  {
+    "revision": "7b4f849e958ac4107787",
+    "url": "/robofriends/static/js/runtime-main.a1071882.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
